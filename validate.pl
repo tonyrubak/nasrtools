e7880da02bf5d96126ad06a48cb3e1f8293f8eae
Joe Peralta <joe@crimsonhexagon.us>
2019-06-11 16:15:31 -0400

    Changed to get filenames from command line rather than hardcoded and added usage

--- validate.pl
+++ validate.pl
@@ -3,8 +3,13 @@
 use warnings 'all';
 use strict;
 
-my $fixes = "FIX.csv";
-my $site = "dtw_52_190510_SYS1.st";
+if ($#ARGV != 1) {
+    print "\nUsage: validate.pl datafile sitefile\n";
+    exit;
+}
+
+my $fixes = $ARGV[0];
+my $site = $ARGV[1];
 
 open(my $fh, '<', $fixes) or die("Can't open '$fixes': $!");
 my @fixarr = <$fh>;
@@ -19,8 +24,9 @@ while (my $line = <$sfh>) {
 
         foreach my $fix (@fixarr) {
             chomp($fix);
-            my @fix_data = ($fix =~ /([A-Z0-9]+),([NS]\d+ \d+ \d+),([EW]\d+ \d+ \d+),.*/);
+            my @fix_data = ($fix =~ /([A-Z0-9]+),([NS]\d+ \d+ \d+),([EW]\d+ \d+ \d+)/);
             # if (@fix_data) {
+            if (!@fix_data) { die("Uh oh"); }
                 if ($fix_data[0] eq $point[0]) {
                     $found = 1;
                     if ($point[1] eq $fix_data[1] and $point[2] eq $fix_data[2]) {
